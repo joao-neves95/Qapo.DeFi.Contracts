a78a08e5549cd7cd27dcfe5ecd8116dcd7dd76c1
João Neves <joao95neves@gmail.com>
2021-12-01 23:14:54 +0000

    [Interfaces/External/IUniswapV2Pair.sol]: Removed unnecessary methods

--- src/Interfaces/External/IUniswapV2Pair.sol
+++ src/Interfaces/External/IUniswapV2Pair.sol
@@ -5,10 +5,7 @@ pragma solidity ^0.8.9;
 
 interface IUniswapV2Pair {
 
-    function name() external pure returns (string memory);
-    function symbol() external pure returns (string memory);
     function decimals() external pure returns (uint8);
-    function totalSupply() external view returns (uint);
     function balanceOf(address owner) external view returns (uint);
     function allowance(address owner, address spender) external view returns (uint);
 
@@ -16,16 +13,9 @@ interface IUniswapV2Pair {
     function transfer(address to, uint value) external returns (bool);
     function transferFrom(address from, address to, uint value) external returns (bool);
 
-    function DOMAIN_SEPARATOR() external view returns (bytes32);
-    function PERMIT_TYPEHASH() external pure returns (bytes32);
-    function nonces(address owner) external view returns (uint);
-
-    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
-
-    function MINIMUM_LIQUIDITY() external pure returns (uint);
-    function factory() external view returns (address);
     function token0() external view returns (address);
     function token1() external view returns (address);
+
     function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
     function price0CumulativeLast() external view returns (uint);
     function price1CumulativeLast() external view returns (uint);
@@ -35,7 +25,5 @@ interface IUniswapV2Pair {
     function burn(address to) external returns (uint amount0, uint amount1);
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
     function skim(address to) external;
-    function sync() external;
 
-    function initialize(address, address) external;
 }
